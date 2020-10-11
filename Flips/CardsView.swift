90eacc85dc3e0c60f2b7332212fe306f662425be
Jason R Tibbetts <jason@tibbetts.net>
2020-10-10 22:52:40 -0400

    The CardsView now has a top-level NavigationView. Tapping a cell pushes a stub view for adding a verb.

--- Flips/CardsView.swift
+++ Flips/CardsView.swift
@@ -3,6 +3,16 @@
 import CoreData
 import SwiftUI
 
+struct AddVerbView: View {
+
+    var body: some View {
+        VStack {
+            Text("This is the AddVerbView")
+        }
+    }
+
+}
+
 struct CardsView: View {
 
     @Environment(\.managedObjectContext) private var viewContext
@@ -13,48 +23,41 @@ struct CardsView: View {
     private var verbs: FetchedResults<Verb>
 
     var body: some View {
-        if verbs.isEmpty {
-            VStack {
-                Text("No verbs have been added yet.")
-
-                Button(action: addVerb) {
-                    Label("Add Verb", systemImage: "plus")
+        NavigationView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))],
+                      alignment: .center, spacing: 20) {
+                NavigationLink(destination: AddVerbView()) {
+                    Text("Placeholder")
                 }
             }
-        } else {
-            NavigationView {
-                List {
-                    ForEach(verbs) { verb in
-                        Text(verbatim: verb.root ?? "(none)")
-                    }
-                }
-                .toolbar {
-                    #if os(iOS)
-                    EditButton()
-                    #endif
-                    
-                    Button(action: addVerb) {
-                        Label("Add Verb", systemImage: "plus")
-                    }
-                }
-            }.navigationTitle("Irish Verbs")
+//            .toolbar {
+//                HStack {
+//                    //                #if os(iOS)
+//                    //                EditButton()
+//                    //                #endif
+//
+//                    Button(action: addVerb) {
+//                        Label("Add Verb", systemImage: "plus")
+//                    }
+//                }
+//            }
         }
     }
 
-    private func addVerb() {
-        withAnimation {
-            let newVerb = Verb(context: viewContext)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+//    private func addVerb() {
+//        withAnimation {
+//            let newVerb = Verb(context: viewContext)
+//
+//            do {
+//                try viewContext.save()
+//            } catch {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
+//    }
 
 }
 
--- Flips/FlipsApp.swift
+++ Flips/FlipsApp.swift
@@ -12,4 +12,5 @@ struct FlipsApp: App {
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
+
 }
