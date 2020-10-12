1421c97a937be6bafb35ce9b7194afc383bbcd85
Jason R Tibbetts <jason@tibbetts.net>
2020-10-11 21:32:54 -0400

    Seeded the verb database with two entries.

--- Flips/FlipsApp.swift
+++ Flips/FlipsApp.swift
@@ -6,6 +6,14 @@ import SwiftUI
 struct FlipsApp: App {
     let persistenceController = PersistenceController.shared
 
+    init() {
+        let ritheannVerb = Verb(context: persistenceController.container.viewContext)
+        ritheannVerb.root = "rith"
+
+        let imrionnVerb = Verb(context: persistenceController.container.viewContext)
+        imrionnVerb.root = "imr"
+    }
+
     var body: some Scene {
         WindowGroup {
             CardsView()
