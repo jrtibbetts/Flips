3ef22e73d9bd9dde3e911a6486ee3ad27914b485
Jason R Tibbetts <jason@tibbetts.net>
2020-10-13 00:59:03 -0400

    The VerbConjugationView gets some of its endings from the VerbEndings chart.

--- Flips/VerbConjugationView.swift
+++ Flips/VerbConjugationView.swift
@@ -4,11 +4,11 @@ import SwiftUI
 
 struct InflectionCell: View {
 
-    var ending: String = ""
+    var ending: String? = ""
 
-    var pronoun: String = ""
+    var pronoun: String? = ""
 
-    var root: String = ""
+    var root: String? = ""
 
     var body: some View {
         GeometryReader { (proxy) in
@@ -16,17 +16,17 @@ struct InflectionCell: View {
 
             HStack {
                 HStack {
-                    Text(root)
+                    Text(root ?? "")
                         .font(.body)
                         .multilineTextAlignment(.trailing)
-                    Text(ending)
+                    Text(ending ?? "" )
                         .font(.body)
                         .fontWeight(.bold)
                         .multilineTextAlignment(.leading)
                 }
 //                .frame(width: proxyWidth * 0.8)
 
-                Text(pronoun)
+                Text(pronoun ?? "")
 //                    .frame(width: proxyWidth * 0.2)
             }
 
@@ -38,20 +38,14 @@ struct InflectionTableRow: View {
 
     var root: String = ""
 
-    var singular: (ending: String, pronoun: String)
+    var singular: (ending: String?, pronoun: String)
 
-    var plural: (ending: String, pronoun: String)
+    var plural: (ending: String?, pronoun: String)
 
     var body: some View {
-        GeometryReader { (proxy) in
-            let proxyWidth = proxy.size.width
-
-            HStack {
-                InflectionCell(ending: singular.ending, pronoun: singular.pronoun, root: root)
-//                    .frame(width: proxyWidth * 0.5)
-                InflectionCell(ending: plural.ending, pronoun: plural.pronoun, root: root)
-//                    .frame(width: proxyWidth * 0.5)
-           }
+        HStack {
+            InflectionCell(ending: singular.ending, pronoun: singular.pronoun, root: root)
+            InflectionCell(ending: plural.ending, pronoun: plural.pronoun, root: root)
         }
     }
 
@@ -73,15 +67,15 @@ struct VerbConjugationView: View {
                     .font(.title2)
 
                 InflectionTableRow(root: verb.root!,
-                                   singular: ("im", ""),
-                                   plural: ("imid", ""))
+                                   singular: (Verb.endings[Verb.Ending(person: .first,  number: .singular, tense: .present, mood: .indicative, voice: .active, declension: .first, syllables: .single, vowel: .slender)], ""),
+                                   plural:   (Verb.endings[Verb.Ending(person: .first,  number: .plural,   tense: .present, mood: .indicative, voice: .active, declension: .first, syllables: .single, vowel: .slender)], ""))
                 InflectionTableRow(root: verb.root!,
-                                   singular: ("eann", "tú"),
-                                   plural: ("eann", "sibh"))
+                                   singular: (Verb.endings[Verb.Ending(person: .second, number: .singular, tense: .present, mood: .indicative, voice: .active, declension: .first, syllables: .single, vowel: .slender)], ""),
+                                   plural:   (Verb.endings[Verb.Ending(person: .second, number: .plural,   tense: .present, mood: .indicative, voice: .active, declension: .first, syllables: .single, vowel: .slender)], ""))
                 InflectionTableRow(root: verb.root!,
-                                   singular: ("eann", "sé/sí"),
-                                   plural: ("eann", "siad"))
-            }
+                                   singular: (Verb.endings[Verb.Ending(person: .third,  number: .singular, tense: .present, mood: .indicative, voice: .active, declension: .first, syllables: .single, vowel: .slender)], ""),
+                                   plural:   (Verb.endings[Verb.Ending(person: .third,  number: .plural,   tense: .present, mood: .indicative, voice: .active, declension: .first, syllables: .single, vowel: .slender)], ""))
+           }
 
             Spacer()
         }
