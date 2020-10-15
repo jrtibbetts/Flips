473a266cb14d590c8ed21cd050cea8765b8ac9a7
Jason R Tibbetts <jason@tibbetts.net>
2020-10-15 01:23:08 -0400

    The FlipsApp verb list is now loaded from an array.

--- Flips/FlipsApp.swift
+++ Flips/FlipsApp.swift
@@ -7,20 +7,19 @@ struct FlipsApp: App {
     let persistenceController = PersistenceController(inMemory: true)
 
     init() {
-        let ritheann = Verb(context: persistenceController.container.viewContext)
-        ritheann.root = "rith"
-        ritheann.rootVowel = "i"
-        ritheann.conjugation = 1
-        ritheann.pastParticiple = "rite"
-        ritheann.verbalNoun = "rith"
-        ritheann.polysyllabic = false
-
-        let olann = Verb(context: persistenceController.container.viewContext)
-        olann.root = "ól"
-        olann.rootVowel = "ó"
-        olann.pastParticiple = "ólta"
-        olann.verbalNoun = "ól"
-        olann.polysyllabic = false
+        [
+            ("rith", "rite", "rith", "i", 1, false),
+            ("ól", "ólta", "ól", "ó", 1, false),
+            ("siúil", "siúlta", "siúl", "ú", 1, false),
+        ].forEach { (verbEntry) in
+            let verb = Verb(context: persistenceController.container.viewContext)
+            verb.root = verbEntry.0
+            verb.pastParticiple = verbEntry.1
+            verb.verbalNoun = verbEntry.2
+            verb.rootVowel = verbEntry.3
+            verb.conjugation = Int16(verbEntry.4)
+            verb.polysyllabic = verbEntry.5
+        }
     }
 
     var body: some Scene {
