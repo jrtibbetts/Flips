0e817ef962d16f64446477a4ae8b58d6cb6a5cee
Jason R Tibbetts <jason@tibbetts.net>
2020-10-24 23:53:04 -0400

    How can I make it update the Verb object whenever a field changes?

--- Flips/Views/VerbEditor.swift
+++ Flips/Views/VerbEditor.swift
@@ -4,22 +4,36 @@ import SwiftUI
 
 struct VerbEditor: View {
 
-    @Binding var verb: Verb
+    @Binding var verb: Verb {
+        didSet {
+            dictionaryForm = verb.dictionaryForm ?? "Dictionary form"
+            root = verb.root ?? "Root"
+            simplePast = verb.simplePastRoot ?? "Simple Past Tense"
+            pastParticiple = verb.pastParticiple ?? "Past Participle"
+            verbalNoun = verb.verbalNoun ?? "Verbal Noun"
+            englishPresent = verb.englishPresent ?? "Present Tense"
+            englishPast = verb.englishPast ?? "Past Tense"
+            pastParticiple = verb.pastParticiple ?? "Past Participle"
+            polysyllabic = verb.polysyllabic
+            conjugation = Verb.Conjugation(rawValue: verb.conjugation) ?? .first
+        }
+    }
 
-    @State var dictionaryForm: String = "Dictionary Form"
-    @State var root: String = "Root"
-    @State var simplePast: String = "Simple Past Tense"
-    @State var pastParticiple: String = "Past Participle"
-    @State var verbalNoun: String = "Verbal Noun"
-    @State var englishPresent: String = "Present Tense"
-    @State var englishPast: String = "Past Tense"
-    @State var englishPastParticiple: String = "Past Participle"
+    @State var dictionaryForm: String = "* Dictionary Form"
+    @State var root: String = "* Root"
+    @State var simplePast: String = "* Simple Past Tense"
+    @State var pastParticiple: String = "* Past Participle"
+    @State var verbalNoun: String = "* Verbal Noun"
+    @State var englishPresent: String = "* Present Tense"
+    @State var englishPast: String = "* Past Tense"
+    @State var englishPastParticiple: String = "* Past Participle"
     @State var polysyllabic: Bool = false
     @State var conjugation: Verb.Conjugation = .first
 
     var body: some View {
         VStack(alignment: .center) {
-            TextField(verb.dictionaryForm ?? dictionaryForm,
+            Text("Is verb nil? \(verb.dictionaryForm == nil ? "yes" : "no")")
+            TextField(verb.dictionaryForm ?? "foo",
                       text: $dictionaryForm)
                 .disableAutocorrection(true)
             TextField(verb.root ?? root,
@@ -42,7 +56,21 @@ struct VerbEditor: View {
 
             Button("Save Changes") {
                 verb.dictionaryForm = dictionaryForm
+                verb.root = root
+                verb.pastParticiple = pastParticiple
+                verb.verbalNoun = verbalNoun
+                verb.englishPresent = englishPresent
+                verb.englishPast = englishPast
+                verb.englishPastParticiple = englishPastParticiple
+                try! PersistenceController.preview.container.viewContext.save()
             }
+
+            Text("Dictionary form: \(verb.dictionaryForm ?? "(none)")")
+            Text("Root: \(verb.root ?? "(none)")")
+            Text("English Present: \(verb.englishPresent ?? "(none)")")
+            Text("English Past: \(verb.englishPast ?? "(none)")")
+//            Text(verb.englishPastParticiple ?? "(none)")
+//            Text("English Past Participle: \(verb.englishPastParticiple ?? "(none)")")
         }
     }
 
