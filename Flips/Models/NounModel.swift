bc02c9ad47df0c4d7a8619cb5ee581b5240e1b1c
Jason R Tibbetts <jason@tibbetts.net>
2021-01-18 21:51:52 -0500

    + Added attributes to the Noun data type. Factored some of the VerbModel out into a protocol that can also be used by the NounModel.

---
+++ Flips/Models/NounModel.swift
@@ -0,0 +1,29 @@
+//  Created by Jason R Tibbetts on 1/18/21.
+
+import CoreData
+import SwiftUI
+
+public struct NounModel: PartOfSpeechModel {
+
+    public var persistenceController = PersistenceController(inMemory: true)
+
+    public init() {
+        do {
+            try lines(fromFilename: "nouns", ext: "csv").forEach { (line) in
+                let elements = line.split(separator: ",").map { String($0) }
+                let noun = Noun(context: persistenceController.container.viewContext)
+                noun.root = elements[0]
+                noun.gender = elements[1]
+                noun.declension = Int16(elements[2]) ?? 1
+                noun.genitive = elements[3]
+                noun.plural = elements[4]
+                noun.englishTranslation = elements[5]
+            }
+
+            try persistenceController.container.viewContext.save()
+        } catch {
+            print("Error importing nouns: \(error.localizedDescription)")
+        }
+    }
+
+}
---
+++ Flips/Models/VerbModel.swift
@@ -0,0 +1,80 @@
+//  Created by Jason R Tibbetts on 10/7/20.
+
+import CoreData
+import SwiftUI
+
+public protocol PartOfSpeechModel {
+
+    var persistenceController: PersistenceController { get }
+
+}
+
+public extension PartOfSpeechModel {
+
+    var viewContext: NSManagedObjectContext {
+        return persistenceController.container.viewContext
+    }
+
+    func lines(fromFilename filename: String, ext: String?) throws -> [String] {
+        if let verbUrl = Bundle.main.url(forResource: filename, withExtension: ext) {
+            return try String(contentsOf: verbUrl, encoding: .utf8) // full file
+                .components(separatedBy: CharacterSet.newlines)     // lines
+                .compactMap { $0.trimmed }
+        } else {
+            return []
+        }
+    }
+
+}
+
+public extension String {
+
+    var stringOrNil: String? {
+        return (self.lowercased() == "nil" || self.isEmpty) ? nil : self
+    }
+
+    var trimmed: String? {
+        let trimmedLine = self.trimmingCharacters(in: .whitespaces)
+
+        if trimmedLine.isEmpty || trimmedLine.starts(with: "#") {
+            return nil
+        } else {
+            return trimmedLine
+        }
+    }
+
+}
+public struct VerbModel: PartOfSpeechModel {
+
+    public var persistenceController = PersistenceController(inMemory: true)
+
+    public init() {
+        do {
+            try lines(fromFilename: "verbs", ext: "csv").forEach { (line) in
+                let elements = line.split(separator: ",").map { String($0) }
+
+                let verb = Verb(context: persistenceController.container.viewContext)
+                verb.dictionaryForm = elements[0]
+                verb.root = elements[1]
+                verb.pastRoot = elements[2].stringOrNil ?? verb.root
+                verb.pastRoot2 = elements[3].stringOrNil ?? verb.pastRoot
+                verb.futureRoot = elements[4].stringOrNil ?? verb.root
+                verb.pastParticiple = elements[5]
+                verb.verbalNoun = elements[6]
+                verb.rootVowel = elements[7]
+                verb.conjugation = Int16(elements[8])!
+                verb.polysyllabic = Bool(elements[9])!
+                verb.transitive = Bool(elements[10])!
+                verb.irregular = Bool(elements[11])!
+                verb.englishPresent = elements[12]
+                verb.englishPast = elements[13]
+                verb.englishPastParticiple = elements[14]
+            }
+
+            try persistenceController.container.viewContext.save()
+        } catch {
+            print("Error importing verbs: \(error.localizedDescription)")
+        }
+    }
+}
+
