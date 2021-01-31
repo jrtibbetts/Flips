a8303f9a384732f05ec1450ef492202e811bb78c
Jason R Tibbetts <jason@tibbetts.net>
2021-01-31 02:08:14 -0500

    🐠 Moved the Case, Declension, and Gender structs into a new file.

---
+++ Flips/Grammar/Declinable.swift
@@ -0,0 +1,57 @@
+//  Created by Jason R Tibbetts on 1/31/21.
+
+import Foundation
+
+public enum Case: String {
+
+    case accusative
+    case dative
+    case genitive
+    case nominative
+
+}
+
+public enum Declension: String {
+
+    case first
+    case second
+    case third
+    case fourth
+    case fifth
+
+    init(intValue: Int16) {
+        switch intValue {
+        case 1:
+            self = .first
+        case 2:
+            self = .second
+        case 3:
+            self = .third
+        case 4:
+            self = .fourth
+        default:
+            self = .fifth
+        }
+    }
+
+}
+
+public enum Gender: String {
+
+    case feminine
+    case masculine
+    case none
+
+    init(abbreviation: String) {
+        switch abbreviation.lowercased() {
+        case "f":
+            self = .feminine
+        case "m":
+            self = .masculine
+        default:
+            self = .none
+        }
+
+    }
+
+}
--- Flips/Grammar/NounInflector.swift
+++ Flips/Grammar/NounInflector.swift
@@ -2,60 +2,6 @@
 
 import Foundation
 
-public enum Case: String {
-
-    case accusative
-    case dative
-    case genitive
-    case nominative
-
-}
-
-public enum Declension: String {
-
-    case first
-    case second
-    case third
-    case fourth
-    case fifth
-
-    init(intValue: Int16) {
-        switch intValue {
-        case 1:
-            self = .first
-        case 2:
-            self = .second
-        case 3:
-            self = .third
-        case 4:
-            self = .fourth
-        default:
-            self = .fifth
-        }
-    }
-
-}
-
-public enum Gender: String {
-
-    case feminine
-    case masculine
-    case none
-
-    init(abbreviation: String) {
-        switch abbreviation.lowercased() {
-        case "f":
-            self = .feminine
-        case "m":
-            self = .masculine
-        default:
-            self = .none
-        }
-
-    }
-
-}
-
 public class NounInflector: NSObject, ObservableObject {
 
     @Published open var declension: Declension
