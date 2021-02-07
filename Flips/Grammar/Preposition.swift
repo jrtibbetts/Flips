805a969f974c88475ffb82f6face17bd8ef48029
Jason R Tibbetts <jason@tibbetts.net>
2021-02-06 23:55:21 -0500

    + Added a Preposition protocol.

---
+++ Flips/Grammar/Preposition.swift
@@ -0,0 +1,31 @@
+//  Created by Jason R Tibbetts on 2/6/21.
+
+import Foundation
+
+public protocol Preposition {
+
+}
+
+public struct Ag: Preposition {
+
+}
+
+public struct Ar: Preposition {
+
+}
+
+public struct Chuig: Preposition {
+
+}
+
+public struct Do: Preposition {
+
+}
+
+public struct I: Preposition {
+
+}
+
+public struct Le: Preposition {
+
+}
