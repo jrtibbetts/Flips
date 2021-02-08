b61338b06cd697f4e661fb3f016e34027cd78c18
Jason R Tibbetts <jason@tibbetts.net>
2021-02-07 23:29:44 -0500

    + Fleshed out some of the prepositions' decline(declinedNoun:) functions.

--- Flips/Grammar/Preposition.swift
+++ Flips/Grammar/Preposition.swift
@@ -4,28 +4,72 @@ import Foundation
 
 public protocol Preposition {
 
+    var governsCase: Case { get }
+
+    func decline(declinedNoun: String) -> String?
+
+//    func declineWithDefiniteArticle(noun: Noun, number: Verb.Number) -> String?
+//
+//    func declineWithPossessive(noun: Noun, possessive: String, person: Verb.Person, number: Verb.Number) -> String?
+
 }
 
 public struct Ag: Preposition {
+
+    public var governsCase = Case.dative
+
+    public func decline(declinedNoun: String) -> String? {
+        return "ag " + declinedNoun
+    }
 
 }
 
 public struct Ar: Preposition {
 
+    public var governsCase = Case.dative
+
+    public func decline(declinedNoun: String) -> String? {
+        return "ar " + declinedNoun
+    }
+
 }
 
 public struct Chuig: Preposition {
+
+    public var governsCase = Case.dative
+
+    public func decline(declinedNoun: String) -> String? {
+        return "chuig " + declinedNoun
+    }
 
 }
 
 public struct Do: Preposition {
 
+    public var governsCase = Case.dative
+
+    public func decline(declinedNoun: String) -> String? {
+        return "do " + declinedNoun
+    }
+
 }
 
 public struct I: Preposition {
 
+    public var governsCase = Case.dative
+
+    public func decline(declinedNoun: String) -> String? {
+        return "i " + declinedNoun
+    }
+
 }
 
 public struct Le: Preposition {
+
+    public var governsCase = Case.dative
+
+    public func decline(declinedNoun: String) -> String? {
+        return "le " + declinedNoun
+    }
 
 }
