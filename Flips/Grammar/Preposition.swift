bc57856758eefc3677aa0de39908b732378bcf87
Jason R Tibbetts <jason@tibbetts.net>
2021-02-11 01:03:51 -0500

    + Added some more InflectedPrepositions.

--- Flips/Grammar/Preposition.swift
+++ Flips/Grammar/Preposition.swift
@@ -238,12 +238,158 @@ public struct Chuig: InflectedPreposition {
 
 }
 
-public struct Do: Preposition {
+public struct De: InflectedPreposition {
+
+    public var governsCase = Case.dative
+
+    public func decline(declinedNoun: String) -> String? {
+        return "de " + declinedNoun
+    }
+
+    public func inflect(number: Grammar.Number, person: Grammar.Person, gender: Gender) -> String? {
+        switch (number, person, gender) {
+        case (.singular, .first, _):
+            return "díom"
+        case (.singular, .second, _):
+            return "díot"
+        case (.singular, .third, .masculine):
+            return "de"
+        case (.singular, .third, .feminine):
+            return "di"
+        case (.plural, .first, _):
+            return "dínn"
+        case (.plural, .second, _):
+            return "díbh"
+        case (.plural, .third, _):
+            return "díobh"
+        default:
+            return nil
+        }
+    }
+
+    public func inflectEmphatic(number: Grammar.Number, person: Grammar.Person, gender: Gender) -> String? {
+        switch (number, person, gender) {
+        case (.singular, .first, _):
+            return "díomsa"
+        case (.singular, .second, _):
+            return "díotsa"
+        case (.singular, .third, .masculine):
+            return "desean"
+        case (.singular, .third, .feminine):
+            return "dise"
+        case (.plural, .first, _):
+            return "dínne"
+        case (.plural, .second, _):
+            return "díbhe"
+        case (.plural, .third, _):
+            return "díobhsan"
+        default:
+            return nil
+        }
+    }
+
+}
+
+public struct Do: InflectedPreposition {
 
     public var governsCase = Case.dative
 
     public func decline(declinedNoun: String) -> String? {
         return "do " + declinedNoun
+    }
+
+    public func inflect(number: Grammar.Number, person: Grammar.Person, gender: Gender) -> String? {
+        switch (number, person, gender) {
+        case (.singular, .first, _):
+            return "dom"
+        case (.singular, .second, _):
+            return "duit"
+        case (.singular, .third, .masculine):
+            return "dó"
+        case (.singular, .third, .feminine):
+            return "di"
+        case (.plural, .first, _):
+            return "dúinn"
+        case (.plural, .second, _):
+            return "daoibh"
+        case (.plural, .third, _):
+            return "dóibh"
+        default:
+            return nil
+        }
+    }
+
+    public func inflectEmphatic(number: Grammar.Number, person: Grammar.Person, gender: Gender) -> String? {
+        switch (number, person, gender) {
+        case (.singular, .first, _):
+            return "domsa"
+        case (.singular, .second, _):
+            return "duitse"
+        case (.singular, .third, .masculine):
+            return "dósan"
+        case (.singular, .third, .feminine):
+            return "dise"
+        case (.plural, .first, _):
+            return "dúinne"
+        case (.plural, .second, _):
+            return "daoibhse"
+        case (.plural, .third, _):
+            return "dóibhsean"
+        default:
+            return nil
+        }
+    }
+
+}
+
+public struct Faoi: InflectedPreposition {
+
+    public var governsCase = Case.dative
+
+    public func decline(declinedNoun: String) -> String? {
+        return "faoi " + declinedNoun
+    }
+
+    public func inflect(number: Grammar.Number, person: Grammar.Person, gender: Gender) -> String? {
+        switch (number, person, gender) {
+        case (.singular, .first, _):
+            return "fúm"
+        case (.singular, .second, _):
+            return "fút"
+        case (.singular, .third, .masculine):
+            return "faoi"
+        case (.singular, .third, .feminine):
+            return "fúithi"
+        case (.plural, .first, _):
+            return "fúinn"
+        case (.plural, .second, _):
+            return "fúibh"
+        case (.plural, .third, _):
+            return "fúthu"
+        default:
+            return nil
+        }
+    }
+
+    public func inflectEmphatic(number: Grammar.Number, person: Grammar.Person, gender: Gender) -> String? {
+        switch (number, person, gender) {
+        case (.singular, .first, _):
+            return "fúmsa"
+        case (.singular, .second, _):
+            return "fútsa"
+        case (.singular, .third, .masculine):
+            return "faoisean"
+        case (.singular, .third, .feminine):
+            return "fúithise"
+        case (.plural, .first, _):
+            return "fúinne"
+        case (.plural, .second, _):
+            return "fúibhse"
+        case (.plural, .third, _):
+            return "fúthusean"
+        default:
+            return nil
+        }
     }
 
 }
