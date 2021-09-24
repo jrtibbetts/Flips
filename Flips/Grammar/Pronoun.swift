1c680bd95652b018ff0575b011f70bd1f3087f18
Jason R Tibbetts <jason@tibbetts.net>
2021-09-24 14:05:50 -0400

    No ticket: [add] Added PossessivePronoun.

---
+++ Flips/Grammar/Pronoun.swift
@@ -0,0 +1,51 @@
+//  Created by Jason R Tibbetts on 9/24/21.
+
+import Foundation
+
+public enum PossessivePronoun {
+
+    case firstSingular
+
+    case secondSingular
+
+    case thirdSingularMasculine
+
+    case thirdSingularFeminine
+
+    case firstPlural
+
+    case secondPlural
+
+    case thirdPlural
+
+    var string: String {
+        switch self {
+        case .firstSingular:
+            return "mo"
+        case .secondSingular:
+            return "do"
+        case .firstPlural:
+            return "ár"
+        case .secondPlural:
+            return "bhur"
+        default:
+            return "a:"
+        }
+    }
+
+    func modify(_ noun: String) -> String {
+        switch self {
+        case .firstSingular,
+                .secondSingular,
+                .thirdSingularMasculine:
+            return noun.lenited
+        case .thirdSingularFeminine:
+            return noun
+        case .firstPlural,
+                .secondPlural,
+                .thirdPlural:
+            return noun.eclipsed
+        }
+    }
+
+}
